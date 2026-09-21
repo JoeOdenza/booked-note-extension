02b62447b88b84d0f05a55d19634f5fc6c00d7b1
Joe Lin <joe@odenz.com>
2026-09-21 10:18:13 -0700

    For the demo

--- src/site-config.ts
+++ src/site-config.ts
@@ -14,8 +14,17 @@ export const config: SiteConfig[] = [
       confirmationNumber:
         'body > div.pRubeObj.pRubeObjInmain > table > tbody > tr > td > table > tbody > tr > td > div > table.confirmation-details.section > tbody > tr:nth-child(1) > td > p:nth-child(1) > span:nth-child(2) > b',
         name: 'body > div.pRubeObj.pRubeObjInmain > table > tbody > tr > td > table > tbody > tr > td > div > table.traveler-accommodation-information.border-bottom > tbody > tr:nth-child(1) > th.traveler-information-cell.column > p:nth-child(2)'
-    },
-  },
+    }, 
+  }, 
+  {
+    hostname: '',
+    pathname: '',
+    includes: ['view-hold'],
+    extract: {
+      finalAmount:
+        'body > div.site-main-container.clearfix > div.main-contained-site > div > div.site-container > div > div.find-my-reservation.contained-item > div > div.col-lg-4 > div > div.trip-summary-container.js-trip-summary-container > div.summary-final-item > div.summary-final-item-amount'
+    }, 
+  }
 ]
 
 export function findMatchingSite(url: string): SiteConfig | undefined {

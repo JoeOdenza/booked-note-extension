f078276372bc82b9f80380da96413a5f74bdebf5
sikij_odenza <sikij@odenza.com>
2026-09-23 14:17:00 -0700

    impl: p&l, agent markup computation

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ function extractPageData() {
   const params = Object.fromEntries(
     new URL(window.location.href).searchParams,
   ) as {
-    profileNum: string;
-    rescardNum: string;
-    certCodeAndNum: string;
+    PROFILENO: string;
+    RESCARD: string;
+    CERT: string;
   };
+
+  const certCode = params.CERT?.match(/[A-Za-z]+/)?.[0] ?? "";
+  const certNum = Number(params.CERT?.match(/[0-9]+/)?.[0]) ?? 0;
 
   const groupCodeRaw = document.querySelector(
     "#pnlCard > table > tbody > tr:nth-child(4) > td:nth-child(2)",
@@ -22,7 +25,31 @@ function extractPageData() {
     document.querySelectorAll<HTMLInputElement>('[id*="CommAmt"]');
   const commAmounts = Array.from(commAmtElements, (elem) => Number(elem.value));
 
-  return { params, groupCode, supplierAmounts, commAmounts };
+  return { certCode, certNum, groupCode, supplierAmounts, commAmounts };
+}
+
+function getExpectedValues(
+  certCode: string,
+  totalSupplierPaid: number,
+  totalComission: number,
+  totalCustomerPayment: number,
+) {
+  const bookingDiff = totalCustomerPayment - totalSupplierPaid;
+  const profitAndLoss = bookingDiff + totalComission;
+
+  let agentMarkup;
+  if (bookingDiff >= 0) {
+    agentMarkup = bookingDiff;
+  } else {
+    const leftOverComission = totalComission + bookingDiff;
+    agentMarkup = Math.max(0, leftOverComission);
+  }
+
+  return {
+    expectedProfitAndLoss: profitAndLoss,
+    expectedAgentMarkup: agentMarkup,
+    expectedGroupCode: lookupGroupCode(certCode),
+  };
 }
 
 function main() {

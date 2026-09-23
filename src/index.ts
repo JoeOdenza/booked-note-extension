ce0123236b50e12ecc4bd6ed359060a3fe9d831c
Joe Lin <joe@odenz.com>
2026-09-23 13:56:36 -0700

    Added red box drawing functionality and listener for page to draw red box

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
 function main() {
+  drawRedBox("#lblBookingPL")
   console.log(window.location.href);
 }
 
 main();
+
+
+// Styling of red box for specified selector
+function drawRedBox(selector: string): boolean {
+  const field = document.querySelector<HTMLElement>(selector)
+
+  if (!field)
+    return false
+
+  field.style.outline = "3px solid red"
+  field.style.outlineOffset = "2px"
+  return true
+}
+
+chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
+  if (message.type === "HIGHLIGHT_SELECTOR") {
+    sendResponse({ found: drawRedBox(message.selector) })
+  }
+})
+

1c08b818972d35966e37b7f831b0c311dfb40352
sikij_odenza <sikij@odenza.com>
2026-09-23 13:45:10 -0700

    impl: booked notes page data extraction

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
+function extractPageData() {
+  const params = Object.fromEntries(
+    new URL(window.location.href).searchParams,
+  ) as {
+    profileNum: string;
+    rescardNum: string;
+    certCodeAndNum: string;
+  };
+
+  const groupCodeRaw = document.querySelector(
+    "#pnlCard > table > tbody > tr:nth-child(4) > td:nth-child(2)",
+  );
+  const groupCode = groupCodeRaw?.textContent?.trim() ?? null;
+
+  const totalFareElements =
+    document.querySelectorAll<HTMLInputElement>('[id*="TotalFare"]');
+  const supplierAmounts = Array.from(totalFareElements, (elem) =>
+    Number(elem.value),
+  );
+
+  const commAmtElements =
+    document.querySelectorAll<HTMLInputElement>('[id*="CommAmt"]');
+  const commAmounts = Array.from(commAmtElements, (elem) => Number(elem.value));
+
+  return { params, groupCode, supplierAmounts, commAmounts };
+}
+
 function main() {
-  console.log(window.location.href);
+  const data = extractPageData();
+  console.log(data);
 }
 
 main();
